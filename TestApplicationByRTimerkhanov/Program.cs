22ef3a19f456fa5cd40487187436403302a97ee7
rtimerkhanov <rrtimerkhanov@gmail.com>
2017-08-10 09:20:23 +0300

    Init

--- TestApplicationByRTimerkhanov/Program.cs
+++ TestApplicationByRTimerkhanov/Program.cs
@@ -10,7 +10,47 @@ namespace TestApplicationByRTimerkhanov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"Здравствуй, дорогой друг!
+Давай я продемонстрирую тебе свой сервис TrueTweetStatisticsNagibator666.
+Желаешь сначала войти в свой аккаунт Twitter, чтобы иметь возможность публиковать результаты?
+1. Да, конечно.
+2. Нет. Возможно, позже.");
+
+            bool isOK = false;
+            int choice = 0;
+
+            do
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1 || choice == 2)
+                    {
+                        isOK = true;
+                    }
+                    else throw new FormatException();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\n\nВероятно, ты допустил ошибку при вводе. Попробуй еще раз, у тебя все получится :)\n");
+                    Console.WriteLine(@"Желаешь сначала войти в свой аккаунт Twitter, чтобы иметь возможность публиковать результаты?
+1. Да, конечно.
+2. Нет. Возможно, позже.");
+                } while (isOK == false);
+
+            switch (choice)
+            {
+                case 1: break;
+                case 2: break;
+            }
+
         }
+
+        static void logIn()
+        {
+
+        }
+
+
+
     }
 }
